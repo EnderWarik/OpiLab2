3be9328584bb0627f8551d74ecf8be0fd88f589d
EnderWarik <Steam1117@mail.ru>
2022-02-17 18:46:03 +1000

    1

--- OPI.cpp
+++ OPI.cpp
@@ -5,9 +5,10 @@
 using namespace std;
 int main()
 {
-    int x, y;
-    cin >> x >> y;
-    cout << x+y;
+    int number, numberTwo;
+    cout << "first";
+    cout << "second";
+    cout << "third";
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
