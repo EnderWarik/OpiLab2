d3e482909ed15b8631722185b08d5086c77b1f36
EnderWarik <Steam1117@mail.ru>
2022-02-19 00:45:23 +1000

    Doxygen documentation

--- Person.h
+++ Person.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <string>
+/*!
+	\brief Parent class that does not carry any semantic load
+
+*/
 class Person
 {
 public:
-	std::string name;
-	std::string age;
+	std::string name; ///< person name
+	std::string age;///< person age
 
 };
 
